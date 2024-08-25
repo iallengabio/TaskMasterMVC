27d083613d9c96de17e1f9853539dafa3957b4dd
Iallen Gábio de Sousa Santos <iallen16@gmail.com>
2024-08-25 11:43:41 -0300

    separating the firebase API key into a file ignored by git

--- src/model/services/util/firebaseInit.ts
+++ src/model/services/util/firebaseInit.ts
@@ -3,14 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { firebaseConfigAPI } from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-
-};
+const firebaseConfig = firebaseConfigAPI;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
